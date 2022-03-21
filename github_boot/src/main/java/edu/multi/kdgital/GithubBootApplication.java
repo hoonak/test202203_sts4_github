43fefb0d10fc8b3412a7eb6cbba96ad5d6c0af45
hoonak <100470460+hoonak@users.noreply.github.com>
2022-03-21 15:24:54 +0900

    Update GithubBootApplication.java

--- github_boot/src/main/java/edu/multi/kdgital/GithubBootApplication.java
+++ github_boot/src/main/java/edu/multi/kdgital/GithubBootApplication.java
@@ -9,6 +9,7 @@ public class GithubBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GithubBootApplication.class, args);
 		System.out.println("깃협push용도");
+		System.out.println("원격저장 추가라인);
 	}
 
 }
