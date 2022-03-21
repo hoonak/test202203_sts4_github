6968a171854946dd45e2378ce4a71f3b1132f955
hoonhak <hoonak2@naver.com>
2022-03-21 15:45:10 +0900

    sts second commit

--- github_boot/src/main/java/edu/multi/kdgital/GithubBootApplication.java
+++ github_boot/src/main/java/edu/multi/kdgital/GithubBootApplication.java
@@ -9,7 +9,8 @@ public class GithubBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GithubBootApplication.class, args);
 		System.out.println("깃협push용도");
-		System.out.println("원격저장 추가라인);
+		System.out.println("원격저장 추가라인");
+		System.out.println("소스주인 추가 라인");
 	}
 
 }
